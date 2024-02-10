5a083aacfca13b886f9b61e4dfef9d02effe93b9
Sophie <sophie@sophiecat.pw>
2024-02-10 00:36:11 +0000

    Add initial waybar configuration

--- hosts/FWL13-L/home.nix
+++ hosts/FWL13-L/home.nix
@@ -8,10 +8,46 @@
     neofetch
   ];
 
-  programs.git = {
-    enable = true;
-    userEmail = "sophie@sophiecat.pw";
-    userName = "Sophie";
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "sophie@sophiecat.pw";
+      userName = "Sophie";
+    };
+    waybar = {
+      enable = true;
+      settings = {
+        height = 30;
+        spacing = 4;
+        modules-left = [ "sway/workspaces" "sway/mode" "sway/scratchpad" "custom/media" ];
+        modules-center = [ "sway/window" ];
+        modules-right = [
+          "mpd"
+          "idle_inhibitor"
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "temperature"
+          "backlight"
+          "keyboard-state"
+          "sway/language"
+          "battery"
+          "battery#bat2"
+          "clock"
+          "tray"
+        ];
+        keyboard-state = {
+          numlock = true;
+          capslock = true;
+          format = "{name} {icon}";
+          format-icons = {
+            locked = "";
+            unlocked = "";
+          };
+        };
+      };
+    };
   };
 
   home.stateVersion = "23.11";
