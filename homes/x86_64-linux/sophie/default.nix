28ffbb979ed0bf201c41e60a6e77761dff016979
Sophie <sophie@sophiecat.pw>
2024-03-25 21:36:08 +0000

    Add hyprland initial module and add to koala

--- homes/x86_64-linux/sophie/default.nix
+++ homes/x86_64-linux/sophie/default.nix
@@ -10,7 +10,7 @@ with lib.custom; {
     };
     wm-tools = {
       wofi.enable = true;
-      sway.enable = true;
+      hyprland.enable = true;
       waybar.enable = true;
     };
     dev = {
---
+++ modules/home/wm-tools/hyprland/default.nix
@@ -0,0 +1,16 @@
+{ lib, config, pkgs, ... }:
+
+with lib;
+with lib.custom; let
+  cfg = config.custom.wm-tools.hyprland;
+in {
+  options.custom.wm-tools.hyprland = with types; {
+    enable = mkBoolOpt false "Whether or not to enable hyprland.";
+  };
+
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+    };
+  };
+}
