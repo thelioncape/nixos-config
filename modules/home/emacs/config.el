b63cd7bd5d24ccf4d97037e96140e2a92b78730d
Sophie <sophie@sophiecat.pw>
2024-06-30 23:07:48 +0100

    Enable catppuccin latte for emacs

--- modules/home/emacs/config.el
+++ modules/home/emacs/config.el
@@ -75,3 +75,5 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 (setq nerd-icons-font-names '("SymbolsNerdFontMono-Regular.ttf"))
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'latte)
