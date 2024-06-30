41d494ca4fabc21bbffda1e4828a57cfa3291c49
Sophie <sophie@sophiecat.pw>
2024-06-30 22:16:48 +0100

    Add animations and decorations to Hyprland

--- modules/home/wm-tools/hyprland/default.nix
+++ modules/home/wm-tools/hyprland/default.nix
@@ -35,6 +35,53 @@ in {
         exec-once = [
           "waybar"
         ];
+        misc = {
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+        };
+        decoration = {
+          rounding = 10;
+          active_opacity = 1.0;
+          inactive_opacity = 0.8;
+          drop_shadow = true;
+          shadow_range = 8;
+          shadow_render_power = 2;
+          blur = {
+            enabled = true;
+            size = 8;
+            passes = 3;
+            noise = 0.01;
+            contrast = 0.9;
+            brightness = 0.8;
+            popups = true;
+          };
+        };
+        animations = {
+          enabled = true;
+          bezier = [
+            "wind, 0.05, 0.9, 0.1, 1.05"
+            "winIn, 0.1, 1.1, 0.1, 1.1"
+            "winOut, 0.3, -0.3, 0, 1"
+            "liner, 1, 1, 1, 1"
+            "siryessir, 0.1, 1.0, 0.1, 1.0"
+          ];
+          animation = [
+            "windows, 1, 6, wind, slide"
+            "windowsIn, 1, 6, winIn, popin"
+            "windowsOut, 1, 5, winOut, slide"
+            "windowsMove, 1, 5, wind, slide"
+            "layers, 1, 5, siryessir, popin"
+            "border, 1, 1, liner"
+            "borderangle, 1, 30, liner, loop"
+            "fade, 1, 10, default"
+            "workspaces, 1, 5, wind"
+            "specialWorkspace, 1, 10, winIn, fade"
+          ];
+        };
+        bindm = [
+          "SUPER, mouse:273, resizewindow"
+          "SUPER, mouse:272, movewindow"
+        ];
         bind = [
           "$mod_SHIFT,E,exit"
           "$mod,Return,exec,$terminal"
@@ -79,8 +126,13 @@ in {
           "$mod,F,fullscreen"
           "$mod_SHIFT,Space,togglefloating"
           # Brightness Controls
+        ];
+        bindl = [
           ",XF86MonBrightnessUp,exec,brillo -u 150000 -A 5"
           ",XF86MonBrightnessDown,exec,brillo -u 150000 -U 5"
+          ",XF86AudioMute,exec,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ",XF86AudioRaiseVolume,exec,wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
         ];
       };
     };
