adeef11e44303118cbe6a3e72c00377dde1786e2
Sophie <sophie@sophiecat.pw>
2024-06-30 23:49:03 +0100

    Create proxmox lxc archetype

---
+++ modules/nixos/archetypes/proxmox-lxc/default.nix
@@ -0,0 +1,25 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.custom; let
+  cfg = config.custom.archetypes.proxmox-lxc;
+in {
+  options.custom.archetypes.proxmox-lxc = with types; {
+    enable = mkBoolOpt false "Whether or not to enable the proxmox-lxc archetype.";
+  };
+
+  config = mkIf cfg.enable {
+    # Supress systemd units that don't work because of LXC
+    systemd.suppressedSystemUnits = [
+      "dev-mqueue.mount"
+      "sys-kernel-debug.mount"
+      "sys-fs-fuse-connections.mount"
+    ];
+    # start tty0 on serial console
+    systemd.services."getty@tty1" = {
+      enable = lib.mkForce true;
+      wantedBy = [ "getty.target" ]; # to start at boot
+      serviceConfig.Restart = "always"; # restart when session is closed
+    };
+    boot.isContainer = true;
+  };
+}
--- systems/x86_64-linux/adguardhome/default.nix
+++ systems/x86_64-linux/adguardhome/default.nix
@@ -2,6 +2,7 @@
 with lib;
 with lib.custom; {
   custom = {
+    archetypes.proxmox-lxc.enable = true;
     suites.server-common.enable = true;
     services.adguardhome.enable = true;
   };
